9c11550748eb269ac2c2a89b0087d53cecb3afa7
LongRangeBehaviorModificationSpecialist <31972048+LongRangeBehaviorModificationSpecialist@users.noreply.github.com>
2024-02-26 19:41:13 -0500

    Update xor.py

--- resources/xor.py
+++ resources/xor.py
@@ -1,5 +1,4 @@
 # !/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 from rich.console import Console
 from pathlib import Path
@@ -21,7 +20,9 @@ class XOREncryption:
         for i in range(len(message)):
             c = message[i]
             key_to_encrypt = xor_key[i % len(xor_key)]
-            encrypted_text += chr(ord(c) ^ ord(key_to_encrypt))
+            encrypted_text += chr(
+                ord(c) ^ ord(key_to_encrypt)
+            )
 
         with open(xor_enc_msg_file, 'w') as f:
             f.write(encrypted_text)
@@ -31,7 +32,8 @@ class XOREncryption:
 **ACTION SUCCESSFUL**\n
 The encrypted message is:\n
     [bright_white]{encrypted_text}[green3]\n
-==========================================""")
+=========================================="""
+        )
 
 
     def encrypt_file_with_xor(self,
@@ -46,7 +48,9 @@ The encrypted message is:\n
             for i in range(len(plain_text)):
                 c = plain_text[i]
                 key_to_encrypt = xor_key[i % len(xor_key)]
-                encrypted_data += chr(ord(c) ^ ord(key_to_encrypt))
+                encrypted_data += chr(
+                    ord(c) ^ ord(key_to_encrypt)
+                )
 
         with open(xor_enc_file, 'w', encoding='utf-8') as f:
             f.write(encrypted_data)
@@ -55,7 +59,8 @@ The encrypted message is:\n
 ==========================================
 **ACTION SUCCESSFUL**\n
 File Encrypted with XOR key
-==========================================""")
+=========================================="""
+        )
 
 
 class XORDecryption:
@@ -70,14 +75,16 @@ class XORDecryption:
         for i in range(len(message)):
             text_to_decrypt = message[i]
             key_to_decrypt = xor_key[i % len(xor_key)]
-            decrypted_message += chr(ord(text_to_decrypt) ^ ord(key_to_decrypt))
-
+            decrypted_message += chr(
+                ord(text_to_decrypt) ^ ord(key_to_decrypt)
+            )
         console.print(f"""[green3]
 ==========================================
 **ACTION SUCCESSFUL**\n
 The original message is:\n
     [bright_white]{decrypted_message}[green3]\n
-==========================================""")
+=========================================="""
+        )
 
 
     def decrypt_file_with_xor(self,
@@ -95,7 +102,9 @@ The original message is:\n
             for i in range(len(encrypted_data)):
                 text_to_decrypt = encrypted_data[i]
                 key_to_decrypt = xor_key[i % len(xor_key)]
-                decrypted_data += chr(ord(text_to_decrypt) ^ ord(key_to_decrypt))
+                decrypted_data += chr(
+                    ord(text_to_decrypt) ^ ord(key_to_decrypt)
+                )
 
         with open(decrypted_file, 'w') as f:
             f.write(decrypted_data)
@@ -104,4 +113,5 @@ The original message is:\n
 ==========================================
 **ACTION SUCCESSFUL**\n
 File Decrypted with XOR key
-==========================================""")
+=========================================="""
+        )
